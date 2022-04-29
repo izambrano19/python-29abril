e0dec9f15ab9e7f533d8e948a590d256b2125f14
super <izambrano19@ilg.cat>
2022-04-29 15:59:02 +0200

    Proyecto-python-29/04/2022

---
+++ main.py
@@ -0,0 +1,21 @@
+import Bio
+from Bio import SeqIO
+from Bio import Entrez
+
+Entrez.email = "izambrano19@ilg.cat"
+
+#handle = Entrez.einfo()
+#result = handle.read()
+#handle.close()
+#print(result)
+
+handle = Entrez.einfo()
+record = Entrez.read(handle)
+print(record.keys())
+llista = record.keys()
+print(llista)
+print(record["DbList"])
+
+for bbdd in llista:
+    print(bbdd)
+#record["DbList"]
